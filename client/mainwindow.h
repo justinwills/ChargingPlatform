665da9cb3549d8c0489fa4a056c83bf3758cc709
justinwills <susenojustin2006@gmail.com>
2026-09-10 02:37:18 +0800

    Delete duplicated variable

--- client/mainwindow.h
+++ client/mainwindow.h
@@ -106,7 +106,6 @@ private:
     int m_lastStationId = -1;
     double m_lastStationLat = 0.0;
     double m_lastStationLng = 0.0;
-    int m_lastStationId = -1;
     QString m_lastStationName;
     QString m_lastStationAddress;
     bool m_navigationStarted = false;
